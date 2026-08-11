a97e6764b80376c3d38af80dd7b3fa52442f82b2
sidra3766 <sidrashaikhss06@gmail.com>
2026-08-11 22:18:02 +0530

    Create 1047.java

---
+++ 1047.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Deque <Character> stack = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            }else {
+                stack.push(c);
+            }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+        
+    }
+}
